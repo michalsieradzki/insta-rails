ca8a20271ca053d26bbb0f36ee892d87bcf6918a
michalsieradzki <michal.sieradzki@mail.com>
2024-03-29 11:26:57 +0100

    init app with bootstrap via importmaps

---
+++ config/importmap.rb
@@ -0,0 +1,10 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.1/app/assets/javascripts/rails-ujs.esm.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js", preload: true
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.esm.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
